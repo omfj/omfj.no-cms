226d2d38420fea24b277ec9acbb523740e94b962
Ole Magnus Fon Johnsen <me@omfj.no>
2022-11-10 13:51:28 +0100

    remove unused fields

--- schemas/category.js
+++ schemas/category.js
@@ -1,54 +1,49 @@
 export default {
-  name: 'category',
-  title: 'Category',
-  type: 'document',
+  name: "category",
+  title: "Category",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-      validation: Rule => Rule.required(),
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'emoji',
-      title: 'Emoji',
-      type: 'string',
+      name: "emoji",
+      title: "Emoji",
+      type: "string",
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 96,
       },
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'color',
-      title: 'Color',
-      type: 'color',
+      name: "color",
+      title: "Color",
+      type: "color",
       options: {
         disableAlpha: true,
-      }
-    },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
+      },
     },
   ],
 
   preview: {
     select: {
-      title: 'title',
-      emoji: 'emoji',
+      title: "title",
+      emoji: "emoji",
     },
     prepare(selection) {
-      const {title, emoji} = selection;
+      const { title, emoji } = selection;
       return {
         title: `${emoji ?? ""} ${title}`,
-      }
+      };
     },
-  }
-}
+  },
+};
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,20 +1,13 @@
-import createSchema from 'part:@sanity/base/schema-creator'
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import externalLink from './externalLink'
+import externalLink from "./externalLink";
 
-import category from './category'
-import post from './post'
-import author from './author'
-import project from './project'
+import category from "./category";
+import author from "./author";
+import project from "./project";
 
 export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([
-    project,
-    post,
-    category,
-    author,
-    externalLink,
-  ]),
-})
+  name: "default",
+  types: schemaTypes.concat([project, category, author, externalLink]),
+});
